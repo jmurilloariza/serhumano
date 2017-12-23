679d211d21450385defb9b97af6e61441fa8e3ad
jmurilloariza <jmurilloariza@gmail.com>
2017-12-23 15:03:49 -0500

    Clase util 23-12 Jeferson

--- app/utility/util.php
+++ app/utility/util.php
@@ -5,7 +5,6 @@ class Util {
     public static function validarString($data){
         foreach ($data as $item) {
             if (!is_string($item) || $item == '') {
-                echo 'string mal';
                 return false;
             }
         }
@@ -15,7 +14,6 @@ class Util {
     public static function validarNumber($data){
         foreach ($data as $item) {
             if (!is_numeric($item)) {
-                echo 'number mal';
                 return false;
             }
         }
@@ -26,7 +24,6 @@ class Util {
         foreach ($data as $item) {
             $valores = explode('-', $item);
             if(count($valores) != 3 && !checkdate($valores[1], $valores[2], $valores[0])){
-                echo 'date mal';
                 return false;
             }
         }
